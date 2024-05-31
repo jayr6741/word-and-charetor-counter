b24ea85407c62cbf99950ae841eaf25ef57e74c1
jayr6741 <146107060+jayr6741@users.noreply.github.com>
2024-05-31 22:00:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// change = () => {
-//   let str = document.getElementById("txt").value;
-//   let ans = str.trim();
-//   let ary = str.split(" ");
-//   let x = ary.filter((x) => x);
-//   let arylength = x.length;
-//   let strlen = str.length;
-//     document.getElementById("out").innerHTML=`${arylength} word ${strlen} charetor`
-// };
-
 change = () => {
   let str = document.getElementById("txt").value;
   let ans = str.trim();
   let ary = str.split(" ");
-  let arylength = ary.length;
+  let x = ary.filter((x) => x);
+  let arylength = x.length;
   let strlen = str.length;
-    if(ary.length===""){
-        return;
-    }
-    
     document.getElementById("out").innerHTML=`${arylength} word ${strlen} charetor`
 };
+
+// change = () => {
+//   let str = document.getElementById("txt").value;
+//   let ans = str.trim();
+//   let ary = str.split(" ");
+//   let arylength = ary.length;
+//   let strlen = str.length;
+//     if(ary.length===""){
+//         return;
+//     }
+    
+//     document.getElementById("out").innerHTML=`${arylength} word ${strlen} charetor`
+// };
