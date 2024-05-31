2094d78d58d0fa52110a0d636e2e1c9af537e169
jayr6741 <146107060+jayr6741@users.noreply.github.com>
2024-05-31 21:55:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ change = () => {
   let strlen = str.length;
     document.getElementById("out").innerHTML=`${arylength} word ${strlen} charetor`
 };
+
+// change = () => {
+//   let str = document.getElementById("txt").value;
+//   let ans = str.trim();
+//   let ary = str.split(" ");
+//   let arylength = ary.length;
+//   let strlen = str.length;
+//     if(ary.length===""){
+//         return;
+//     }
+    
+//     document.getElementById("out").innerHTML=`${arylength} word ${strlen} charetor`
+// };
